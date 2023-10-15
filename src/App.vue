2e1030581da1c349a21e6816e9b3742ce2973a04
lvy <1512501934@qq.com>
2023-10-16 00:45:16 +0800

    feat: init

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,8 @@
 
 <template>
   <div>123</div>
+  <p class="text-green text-xl">Green Large</p>
+  <n-button>naive-ui</n-button>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,67 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    UnoCSS(),
+    AutoImport({
+      dts: './auto-imports.d.ts',
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+      // Auto import for module exports under directories
+      // by default it only scan one level of modules under the directory
+      dirs: [
+        // './hooks',
+        // './composables' // only root modules
+        // './composables/**', // all nested modules
+        // ...
+      ],
+      imports: [
+        'vue',
+        'vue-router',
+        'pinia',
+        {
+          '@vueuse/core': [
+            // named imports
+            'useMouse', // import { useMouse } from '@vueuse/core',
+            // alias
+            ['useFetch', 'useMyFetch'] // import { useFetch as useMyFetch } from '@vueuse/core',
+          ],
+          axios: [
+            // default imports
+            ['default', 'axios'] // import { default as axios } from 'axios',
+          ],
+          'naive-ui': [
+            'useDialog',
+            'useMessage',
+            'useNotification',
+            'useLoadingBar'
+          ]
+        },
+
+        //引入类型
+        {
+          from: 'vue-router',
+          imports: ['RouteLocationRaw'],
+          type: true
+        }
+      ],
+
+      // Enable auto import by filename for default module exports under directories
+      defaultExportByFilename: false
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()]
+    })
+  ]
 })
