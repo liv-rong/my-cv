446ef7509ba98648c8e2c6d2c1ef1f53d6f394d4
lvy <upwardsrwr@gmail.com>
2024-05-03 09:43:27 +0800

    feat: pinia

---
+++ src/store/data.ts
@@ -0,0 +1,39 @@
+import { defineStore } from 'pinia'
+import type { SystemData } from '@/types/index'
+
+export const useDataStore = defineStore('data', () => {
+  const data = reactive<SystemData>({
+    mdContent: '',
+    cssContent: '',
+    mdFlag: false,
+    cssFlag: false,
+    curResumeId: null,
+    curResumeName: 'DEFAULT_NAME'
+  })
+
+  const setData = <T extends keyof SystemData>(
+    key: T,
+    value: SystemData[T]
+  ) => {
+    data[key] = value
+    // if (key === 'cssContent') setBackboneCss(value as string, 'preview')
+  }
+
+  const toggleMdFlag = (to: boolean) => {
+    data.mdFlag = to
+  }
+
+  const toggleCssFlag = (to: boolean) => {
+    data.cssFlag = to
+  }
+
+  return {
+    data,
+    setData,
+    toggleMdFlag,
+    toggleCssFlag
+  }
+})
+
+// if (import.meta.hot)
+//   import.meta.hot.accept(acceptHMRUpdate(useDataStore, import.meta.hot))
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export type SystemData = {
+  mdContent: string
+  cssContent: string
+  mdFlag: boolean
+  cssFlag: boolean
+  curResumeId: string | null
+  curResumeName: string
+}
